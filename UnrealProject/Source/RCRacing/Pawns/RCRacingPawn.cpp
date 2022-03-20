28645b3167294d6f704799b3689e9295b4926e2a
Dracrius <a.achorn@gmail.com>
2022-03-19 23:58:05 -0400

    Alpha 1 Complete! Ah ha found the Build Error the OnOverlapBegin was removed from the RCRacingPawn as it had no code in it. This appears to cause build errors as the powerups use it.

--- UnrealProject/Source/RCRacing/Pawns/RCRacingPawn.cpp
+++ UnrealProject/Source/RCRacing/Pawns/RCRacingPawn.cpp
@@ -548,6 +548,12 @@ void ARCRacingPawn::Boost()
 	}
 }
 
+void ARCRacingPawn::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    //Game will fail to build without. This is used for powerups!
+}
+
+
 void ARCRacingPawn::SetupInCarHUD()
 {
 	APlayerController* PlayerController = Cast<APlayerController>(GetController());
