5b67ef79dcde234f5ac3fd55672c2881e19a8ab3
Dracrius <a.achorn@gmail.com>
2022-04-17 00:12:18 -0400

    PlayerController Comments Complete

--- UnrealProject/Source/RCRacing/Pawns/RacingPlayerController.cpp
+++ UnrealProject/Source/RCRacing/Pawns/RacingPlayerController.cpp
@@ -43,16 +43,19 @@ Added Custom PlayerController
 #include "HeadMountedDisplayFunctionLibrary.h"
 #include "Net/UnrealNetwork.h"
 
+//Set The Const Names For Lookup
 const FName ARacingPlayerController::LookUpBinding("LookUp");
 const FName ARacingPlayerController::LookRightBinding("LookRight");
 
+/** Handle Pawn Possesion */
 void ARacingPlayerController::OnPossess(APawn* aPawn)
 {
     Super::OnPossess(aPawn);
-
+    //File Pawn Pointer
     MyPawn = Cast<ARCRacingPawn>(aPawn);
 }
 
+/** Handle Pawn UnPosses */
 void ARacingPlayerController::OnUnPossess()
 {
     APawn* posessedPawn = Cast<APawn>(GetOwner());
@@ -60,43 +63,56 @@ void ARacingPlayerController::OnUnPossess()
     Super::OnUnPossess();
 }
 
+/** Open Pause Menu  */
 void ARacingPlayerController::Pause()
 {
+    //Use Pawns Pause
     if (MyPawn)
         MyPawn->Pause();
 }
 
+/** Handle Forward and Backwards */
 void ARacingPlayerController::MoveForward(float Val)
 {
+    //Use Pawns MoveForward
     if (MyPawn)
         MyPawn->MoveForward(Val);
 }
 
+/** Handle Turning */
 void ARacingPlayerController::MoveRight(float Val)
 {
+    //Use Pawns MoveRight
     if (MyPawn)
         MyPawn->MoveRight(Val);
 }
 
+//switch a few settings and is to call the virtual Use function of the PowerUp class
 void ARacingPlayerController::ServerOnUsePowerUp_Implementation()
 {
+    //Use Pawns OnUsePowerUp
     if (MyPawn)
         MyPawn->ServerOnUsePowerUp();
 }
 
+/** Setup input Comp */
 void ARacingPlayerController::SetupInputComponent()
 {
     Super::SetupInputComponent();
 
+    //Check that InputComp isn't Null
     if (InputComponent != nullptr)
     {
+        //Bind Movement
         InputComponent->BindAxis("MoveForward", this, &ARacingPlayerController::MoveForward);
         InputComponent->BindAxis("MoveRight", this, &ARacingPlayerController::MoveRight);
         InputComponent->BindAxis(LookUpBinding);
         InputComponent->BindAxis(LookRightBinding);
 
+        //Bind Pause Menu
         InputComponent->BindAction("Pause", IE_Pressed, this, &ARacingPlayerController::Pause);
 
+        //Bind Use Powerup
         InputComponent->BindAction("UsePowerUp", IE_Pressed, this, &ARacingPlayerController::ServerOnUsePowerUp);
 
         // VR headset functionality
@@ -104,8 +120,10 @@ void ARacingPlayerController::SetupInputComponent()
     }
 }
 
+/** Resets HMD orientation in VR. */
 void ARacingPlayerController::OnResetVR()
 {
+    //Use Pawns OnResetVR
     if (MyPawn)
         MyPawn->OnResetVR();
 }
--- UnrealProject/Source/RCRacing/Pawns/RacingPlayerController.h
+++ UnrealProject/Source/RCRacing/Pawns/RacingPlayerController.h
@@ -22,32 +22,36 @@ class RCRACING_API ARacingPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-
+    /** Handle Pawn Possesion */
     virtual void OnPossess(APawn* aPawn) override;
+    /** Handle Pawn UnPosses */
     virtual void OnUnPossess() override;
 
-    // End Actor interface
+    /** Open Pause Menu  */
     virtual void Pause();
 
-    /** Handle pressing forwards */
+    /** Handle Forward and Backwards */
     virtual void MoveForward(float Val);
 
-    /** Handle pressing right */
+    /** Handle Turning */
     virtual void MoveRight(float Val);
 
     //switch a few settings and is to call the virtual Use function of the PowerUp class
     UFUNCTION(Server, Reliable)
     virtual void ServerOnUsePowerUp();
 
+    // Binding Names for Lookup
     static const FName LookUpBinding;
     static const FName LookRightBinding;
 
 protected:
+    /** Setup input Comp */
     virtual void SetupInputComponent() override;
 
     /** Resets HMD orientation in VR. */
     virtual void OnResetVR();
 
+    /** To Store the Players Pawn */
     class ARCRacingPawn* MyPawn;
 	
 };
